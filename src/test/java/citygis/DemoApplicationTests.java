a2bfdfd199b636c781ca72fddaec0e78a04899e8
Nav Appaiya <navarajh@gmail.com>
2016-01-18 20:36:01 +0100

    Project build #1

--- src/test/java/citygis/DemoApplicationTests.java
+++ src/test/java/citygis/DemoApplicationTests.java
@@ -1,11 +1,12 @@
 package Citygis;
 
+import nl.citygis.Application;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class DemoApplicationTests {
 
 	@org.testng.annotations.Test
